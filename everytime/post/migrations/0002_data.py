b27e54e837ca084853f8c2d2f41385d6007a0fc5
HeewonJo <heewonjo.me@gmail.com>
2024-12-06 18:02:01 +0900

    Fix: 자잘한 오류 수정

--- everytime/post/migrations/0002_data.py
+++ everytime/post/migrations/0002_data.py
@@ -2,11 +2,16 @@ from django.db import migrations
 
 def create_default_data(apps, schema_editor):
     Category = apps.get_model('post', 'Category')
-    Category.objects.create(title="비밀게시판", slug="secret")
-    Category.objects.create(title="새내기게시판", slug="freshmen")
-    Category.objects.create(title="디지털소프트웨어공학부", slug="deptofcse")
-    Category.objects.create(title="소프트웨어전공", slug="software")
-    Category.objects.create(title="파이썬웹프로그래밍", slug="PWP")
+    if not Category.objects.filter(title="비밀게시판").exists():
+        Category.objects.create(title="비밀게시판", slug="secret")
+    if not Category.objects.filter(title="새내기게시판").exists():
+        Category.objects.create(title="새내기게시판", slug="freshmen")
+    if not Category.objects.filter(title="디지털소프트웨어공학부").exists():
+        Category.objects.create(title="디지털소프트웨어공학부", slug="deptofcse")
+    if not Category.objects.filter(title="소프트웨어전공").exists():
+        Category.objects.create(title="소프트웨어전공", slug="software")
+    if not Category.objects.filter(title="파이썬웹프로그래밍").exists():
+        Category.objects.create(title="파이썬웹프로그래밍", slug="PWP")
 
 class Migration(migrations.Migration):
     dependencies = [
